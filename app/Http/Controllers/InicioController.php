fb9646a1d8fa81a2848801e30cba2b3cae0a47e5
agarcia <agarcia@vennexgroup.com>
2016-07-28 20:36:03 -0500

    Solucion a error de Composer

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -1,6 +1,6 @@
 <?php namespace App\Http\Controllers;
 
-class WelcomeController extends Controller {
+class InicioController extends Controller {
 
 	/*
 	|--------------------------------------------------------------------------
@@ -20,7 +20,7 @@ class WelcomeController extends Controller {
 	 */
 	public function __construct()
 	{
-		$this->middleware('guest');
+		$this->middleware('auth');
 	}
 
 	/**
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', 'InicioController@index');
 
 Route::get('home', 'HomeController@index');
 
