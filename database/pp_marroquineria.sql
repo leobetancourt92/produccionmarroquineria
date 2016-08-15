ca35265711cd6844da4ad92360a2d1d2d3c39db5
agarcia <agarcia@vennexgroup.com>
2016-08-15 14:24:30 -0500

    No borren los commit's

--- database/pp_marroquineria.sql
+++ database/pp_marroquineria.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-08-2016 a las 22:34:16
+-- Tiempo de generación: 15-08-2016 a las 20:38:34
 -- Versión del servidor: 10.1.9-MariaDB
 -- Versión de PHP: 5.6.15
 
